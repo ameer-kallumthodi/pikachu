3e5fa40cd3333d9afdee2b18c903810a2e8b76fb
Prince Rudh <91786052+prince-rudh@users.noreply.github.com>
2022-04-05 22:36:31 +0300

    📢 Rudhra Version 3.0 Available Now! …

---
+++ plugins/number.js
@@ -0,0 +1,19 @@
+//modified @ PrinceRudh
+
+const Prince = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+Prince.addCommand({ pattern: 'number ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+
+//coded by princerudh
+const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+            + 'VERSION:3.0\n' 
+            + 'FN: RUDHRA [BOT OWNER]\n' // full name
+            + 'ORG:₢ᴘʀɪɴᴄᴇ ʀᴜᴅʜ;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid= '+Config.NU+' :+91 9895809960\n' // 
+            + 'END:VCARD'
+await message.client.sendMessage(message.jid,{displayname: "RUDHRA [BOT OWNER]", vcard: vcard}, MessageType.contact , {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "₢ᴘʀɪɴᴄᴇ ʀᴜᴅʜ", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('P3R3R03.jpg')}}}});
+}))
---
+++ plugins/prgitince.js
@@ -0,0 +1,38 @@
+//modified @ princerudh 
+	
+
+const Rudh = require('../events');
+const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const Config = require('../config')
+const axios = require('axios')
+const request = require('request');
+const os = require('os');
+var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
+var ggg = Buffer.from(clh.cd, 'base64')
+var ddd = ggg.toString('utf-8')
+
+let whb = Config.WORKTYPE == 'public' ? false : true
+
+Rudh.addCommand({pattern: 'git', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+// send a buttons message!
+    var plk_say = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
+const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var plk_here = new Date().toLocaleDateString(get_localized_date)
+var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '```'
+	const buttons = [
+
+        {buttonId: 'id1', buttonText: {displayText: '𝗚𝗜𝗧 \n\n\n ┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓ \n      ☆ ~ɪᴛsᴍᴇ ʀᴜᴅʜʀᴀ~ ☆ \n ┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛ \n ┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓ \n ┣⍟ \n ┣⍟ *ᴅᴇᴠᴇʟᴏᴘᴇʀ* : ᴘʀɪɴᴄᴇ ʀᴜᴅʜ \n ┣⍟ \n ┣⍟ *ɴᴜᴍʙᴇʀ* : wa.me/919895809960 \n ┣⍟ \n ┣⍟ *ʙᴏᴛ ɢʀᴏᴜᴘ* https://chat.whatsapp.com/Hgg2xYpPhTHLk77Jcisdq6 \n ┣⍟ \n ┣⍟ *ɪɴsᴛᴀ* :https://instagram.com/princerudh \n ┣⍟ \n ┣⍟ *ʏᴛ* :https://youtube.com/princerudh \n ┣⍟ \n ┣⍟ *ʙᴏᴛ ᴠɪᴅᴇᴏ* :https://youtu.be/zUGBjETc7PA \n ┣⍟ \n ┣⍟ *ɢɪᴛ* :https://github.com/prince-rudh/Rudhra \n ┣⍟ \n ┣⍟ *ɢɪᴛ* : https://github.com/prince-rudh/Rudhra2.0  \n ┣⍟ \n ┣⍟     ▎▍▌▌▉▏▎▌▉▐▏▌▎\n ┣⍟     ▎▍▌▌▉▏▎▌▉▐▏▌▎ \n ┣⍟         ₢ᴘʀɪɴᴄᴇ ʀᴜᴅʜ𖨆 \n ┣⍟ \n ┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛ \n ⍟⍟⍟⍟⍟⍟⍟⍟⍟⍟⍟⍟⍟⍟⍟⍟⍟ \n ┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓ \n ┣⍟        ◩ *ʀᴜᴅʜʀᴀ* ◪ \n ┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛'}, type: 1},
+
+      ]
+      
+      const buttonMessage = {
+          contentText: ' ʜʏ ᴅᴜᴅᴇ....👋🏻\n\n         ☆ *ʙᴏᴛ ɪɴғᴏ* ☆\n\n👾 ɴᴀᴍᴇ  : ' + Config.BOTV2 + '\n👾 sᴛᴀᴛᴇs : ᴘᴜʙʟɪᴄ\n👾 ᴛɪᴍᴇ   : ```' + plk_say + '```\n👾 ᴅᴀᴛᴇ : ```' + plk_here + '```\n👾 ᴘᴇʀғɪx : [ . ]\n\n         ☆ *ᴄʀᴇᴀᴛᴇʀ* ☆\n\n👨🏻‍💻 *ᴘʀɪɴᴄᴇ ʀᴜᴅʜ* \n\n          ☆ *ᴛʜᴀɴᴋ ʏᴏᴜ* ☆\n\n👾 ᴄʟɪᴄᴋ ᴍᴇɴᴜ ᴀɴᴅ ᴇɴᴊᴏʏ ᴛʜᴇ ʙᴏᴛ\n',
+          footerText: '₢ᴘʀɪɴᴄᴇ ʀᴜᴅʜ',
+          buttons: buttons,
+          headerType: 1
+      }
+      
+      await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { mimetype: Mimetype.buttonsMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('P3R3R03.jpg'), surface: 200, message: Config.BOTV2, orderTitle: Config.BOTV2, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOTV2 + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('P3R3R03.jpg')}}}});
+	
+}));
